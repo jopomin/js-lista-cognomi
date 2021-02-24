ad784689dbb5cd6a70f2f66f99470e02c887e8bf
Giovanni <giovannipor23@gmail.com>
2021-02-24 19:26:10 +0100

    Fine Esercizio con Sort

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
-/* creare array con i cognomi dati (var1)
-aggiungere proprio cognome (prompt + push)
-ordinare alfabeticamente i cognomi (?) e inserirli in un secondo array di supporto (var2)
-stampare i cognomi ordinati con numeri da 1 a 6
-(console con ++i o ordered list in html) */
+//creare array con i cognomi dati (var1)
+var listaCognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+console.log(listaCognomi);
+
+//aggiungere proprio cognome (prompt + push)
+var mioCognome = prompt('Inserisci il tuo cognome');
+listaCognomi.push(mioCognome);
+console.log(listaCognomi);
+
+//ordinare alfabeticamente i cognomi (?) e inserirli in un secondo array di supporto (var2)
+var cognomiSorted = listaCognomi.sort();
+console.log(cognomiSorted);
+
+//stampare nella console
+/* for(var i = 0; i < cognomiSorted.length; i++) {
+    console.log((i+1)+" "+cognomiSorted[i]);
+} */
+
+
+//stampare in HTML con numeri "umani"
+for(var i = 0; i < cognomiSorted.length; i++) {
+    document.getElementById("cognomi_ordinati").innerHTML += "<li>" + cognomiSorted[i] + "</li>"
+}
+
+
