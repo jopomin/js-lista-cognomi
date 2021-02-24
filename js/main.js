721aaffabf263df027151c7ad69f517e396f87b5
Giovanni <giovannipor23@gmail.com>
2021-02-24 18:07:54 +0100

    Struttura iniziale e logica commentata

---
+++ js/main.js
@@ -0,0 +1,5 @@
+/* creare array con i cognomi dati (var1)
+aggiungere proprio cognome (prompt + push)
+ordinare alfabeticamente i cognomi (?) e inserirli in un secondo array di supporto (var2)
+stampare i cognomi ordinati con numeri da 1 a 6
+(console con ++i o ordered list in html) */
