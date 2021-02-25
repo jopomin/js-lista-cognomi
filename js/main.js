c9625800940e9ddab0221273881508ee30aa4c6b
Giovanni <giovannipor23@gmail.com>
2021-02-25 10:09:49 +0100

    *RISOLTO* Esercizio con cicli for e confronto dettagliato tra stringhe
    Risolto azzerando il contatore alla fine di ogni secondo ciclo for

--- js/main.js
+++ js/main.js
@@ -16,6 +16,8 @@ console.log(listaCognomi);
     console.log((i+1)+" "+cognomiSorted[i]);
 } */
 
+
+
 //dichiaro una variabile contatore
 var x = 0;
 //creo un array vuoto da riempire con i cognomi ordinati
@@ -26,7 +28,7 @@ for (var a = 0; a < listaCognomi.length; a++) {
     for (var b = 0; b < listaCognomi.length; b++) {
 
 /*         se l'elemento della lista ha un elemento maggiore di sé, aumenta il contatore di uno, altrimenti non fare nulla. Se l'elemento non avrà nessuno maggiore di sé (0), allora sarà esso stesso il maggiore della lista e andrà scritto in posizione "0" del nuovo array, e così via*/
-        if (listaCognomi[a] < listaCognomi[b]) {
+        if (listaCognomi[a] > listaCognomi[b]) {
            x++;
         }
     }
@@ -34,11 +36,15 @@ for (var a = 0; a < listaCognomi.length; a++) {
 console.log(x);
 cognomiSorted[x] = listaCognomi[a];
 console.log(cognomiSorted[x]);
+console.log(cognomiSorted);
+x = 0;
 
 }
+
+
 //stampare in HTML con numeri "umani"
-/* for(var i = 0; i < cognomiSorted.length; i++) {
+for(var i = 0; i < cognomiSorted.length; i++) {
     document.getElementById("cognomi_ordinati").innerHTML += "<li>" + cognomiSorted[i] + "</li>"
-} */
+}
 
 
